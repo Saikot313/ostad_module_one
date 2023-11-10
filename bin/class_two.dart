c1b2356777dab7d4a0139cde74c0d42b85b4e1e5
Md.Sakender Saikot <sakendersaikot88@gmail.com>
2023-11-11 01:27:30 +0600

    all data type

---
+++ bin/class_two.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  String name = 'Md.Sakender Saikot';
+  int age = 23;
+  double hight = 5.8;
+  bool saikotisSingle = true;
+  print(name);
+  print(age);
+  print(hight);
+  print(saikotisSingle);
+
+}
---
+++ test/ostad_module_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:ostad_module_one/ostad_module_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
